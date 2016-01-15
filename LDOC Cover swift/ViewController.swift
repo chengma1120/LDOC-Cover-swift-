6a0d7b9b575fbc33d1d775c7d30ce0a125ede87a
chengma1120 <chengma1120@gmail.com>
2016-01-15 10:36:56 -0500

    test new branch

--- LDOC Cover swift/ViewController.swift
+++ LDOC Cover swift/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.view.backgroundColor = UIColor.lightGrayColor()
+        self.view.backgroundColor = UIColor.darkGrayColor()
         // Do any additional setup after loading the view, typically from a nib.'
     }
 
