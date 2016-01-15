c311ad7a67de9988eda46b36253db860c45800bd
chengma1120 <chengma1120@gmail.com>
2016-01-15 10:34:11 -0500

    Added background view color and background image for button

--- LDOC Cover swift/ViewController.swift
+++ LDOC Cover swift/ViewController.swift
@@ -12,9 +12,15 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.view.backgroundColor = UIColor.lightGrayColor()
+        // Do any additional setup after loading the view, typically from a nib.'
     }
 
+    @IBAction func LineUp(sender: UIButton) {
+        //sender.backgroundColor = UIColor.blackColor()
+       // let buttonImg = UIImage.init(named: "button")
+        //sender.setBackgroundImage(buttonImg, forState: UIControlState.Normal)
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
